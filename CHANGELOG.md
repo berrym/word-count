e2a019b20862013cd24a5d5e2ff86c567c44d2d1
Michael Berry <trismegustis@gmail.com>
2024-12-08 00:21:16 -0500

    wordCount now returns a FileStats record directly.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for word-count
 
+## 0.2.0.0 -- 2024-12-08
+
+* wordCount now returns a FileStats record directly.
+
 ## 0.1.0.0 -- 2024-12-07
 
 * First version. Released on an unsuspecting world.
--- src/Data/WordCount.hs
+++ src/Data/WordCount.hs
@@ -11,7 +11,14 @@ where
 
 import qualified Data.ByteString.Lazy as BS
 
--- | A record to keep the states of the counting.
+-- | A record to store file statistics.
+data FileStats = FileStats
+  { lc :: Int, -- Line count
+    wc :: Int, -- Word count
+    bc :: Int -- File size in bytes
+  }
+
+-- | A record to keep states during counting.
 data State = State
   { ls :: Int,
     ws :: Int,
@@ -20,8 +27,8 @@ data State = State
   }
 
 -- | Count line/words/bytes in a ByteString.
-wordCount :: BS.ByteString -> (Int, Int, Int)
-wordCount s = (ls, ws + 1 - wasSpace, bs)
+wordCount :: BS.ByteString -> FileStats
+wordCount s = FileStats ls (ws + 1 - wasSpace) bs
   where
     State {..} = BS.foldl' go (State 0 0 0 1) s
 
@@ -36,33 +43,10 @@ wordCount s = (ls, ws + 1 - wasSpace, bs)
         incWord = (1 - wasSpace) * isSpace
 {-# INLINE wordCount #-}
 
--- | Get the first element of a three item tuple.
-firstOf3 :: (a, b, c) -> a
-firstOf3 (a, _, _) = a
-
--- | Get the second element of a three item tuple.
-secondOf3 :: (a, b, c) -> b
-secondOf3 (_, b, _) = b
-
--- | Get the third element of a three item tuple.
-thirdOf3 :: (a, b, c) -> c
-thirdOf3 (_, _, c) = c
-
--- | A record to store file statistics.
-data FileStats = FileStats
-  { lc :: Int, -- Line count
-    wc :: Int, -- Word count
-    bc :: Int -- File size in bytes
-  }
-
 -- | Read stats of a file and return the results in a FileStats record.
 readFileStats :: FilePath -> IO FileStats
 readFileStats fp = do
-  contents <- BS.readFile fp -- Read a file into a lazy ByteString
-  let counts = wordCount contents
-  let l = firstOf3 counts
-  let w = secondOf3 counts
-  let b = thirdOf3 counts
-  let fs = FileStats l w b -- Create a FileStat record
-  pure fs -- Return the FileStat record
+  s <- BS.readFile fp -- Read a file into a lazy ByteString.
+  let fs = wordCount s -- Create a FileStats record.
+  pure fs -- Return the FileStats record.
 {-# INLINE readFileStats #-}
